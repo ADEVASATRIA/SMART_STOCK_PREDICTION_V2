feb592d667567b36ab802265be4269e1e481d41d
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-07-01 21:49:54 +0700

    [add]File Awal

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+    
+    @section('title', 'Selamat datang di dashboard Admin - Smart Stock Prediction')
+    
+    @section('contents')
+    <div class="row">
+        Dashboard
+    </div>
+@endsection
