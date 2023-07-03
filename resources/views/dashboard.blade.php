3343920727505d2d8a08a171c783d41920bf73ae
Qii <57295427+syauqqii@users.noreply.github.com>
2023-07-04 01:45:39 +0700

    Biar ga tabrakan

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-    
-    @section('title', 'Selamat datang di dashboard Admin - Smart Stock Prediction')
-    
-    @section('contents')
-    <div class="row">
-        Dashboard
-    </div>
-@endsection
